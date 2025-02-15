0e6b837319bc1315384f1d1b8d272cc4f5d17e4a
yousefhasan <yousefhasan@gmail.com>
2025-02-15 15:53:51 +0200

    Adding Basket Service Mnimal Endpoints

---
+++ Services/Basket/Basket.API/Data/CachedBasketRepository.cs
@@ -0,0 +1,41 @@
+﻿ using Microsoft.Extensions.Caching.Distributed;
+ using System.Text.Json;
+
+namespace Basket.API.Data
+{
+    public class CachedBasketRepository
+        (IBasketRepository Repository, IDistributedCache cache)
+        : IBasketRepository
+    {
+        public async Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellationToken = default)
+        {
+            var baskedCached = await cache.GetStringAsync(userName, cancellationToken);
+            if (!string.IsNullOrEmpty(baskedCached))
+            {
+               return JsonSerializer.Deserialize<ShoppingCart>(baskedCached)!;
+            }
+            var basket = await Repository.GetBasket(userName, cancellationToken);
+
+            await cache.SetStringAsync(userName, JsonSerializer.Serialize(basket), cancellationToken);
+            return basket;
+
+        }
+
+        public async Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellationToken = default)
+        {
+            await Repository.StoreBasket(basket, cancellationToken);
+
+            await cache.SetStringAsync(basket.UserName, JsonSerializer.Serialize(basket), cancellationToken);
+
+            return basket;
+        }
+        public async Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default)
+        {
+            await Repository.DeleteBasket(userName, cancellationToken);
+
+            await cache.RemoveAsync(userName, cancellationToken);
+
+            return true;
+        }
+    }
+}
