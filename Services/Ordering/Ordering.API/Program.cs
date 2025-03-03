8b390f983b3c6eea4a83568f468c2eab59ee184b
yousefhasan <yousefhasan@gmail.com>
2025-03-04 00:56:19 +0200

    Add Ordering API Layer ,Minimal Api Endpoints with Carter and REPR Pattern

--- Services/Ordering/Ordering.API/Program.cs
+++ Services/Ordering/Ordering.API/Program.cs
@@ -9,13 +9,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services
     .AddApplicationServices()
     .AddInfrastructureServices(builder.Configuration)
-    .AddApiServices();
+    .AddApiServices(builder.Configuration);
 
 
 
 var app = builder.Build();
-
-app.UseApiServices();
 
 app.UseApiServices();
 
